b09c2334f9b5928a45daa16ae102088bac18b902
Dagan Martinez <daganboy@gmail.com>
2016-10-20 18:03:44 +0000

    Add tabs

--- Frontend/js/classes.js
+++ Frontend/js/classes.js
@@ -1,38 +1,38 @@
 class Note {
-    constructor(pitch) {
-        this.pitch = pitch;
-    }
+	constructor(pitch) {
+		this.pitch = pitch;
+	}
 }
 class SoundFile {
-    constructor(address) {
-        this.address = address;
-    }
+	constructor(address) {
+		this.address = address;
+	}
 }
 class Clip {
-    constructor(start, duration, content) {
-        /* start time of clip */
-        this.start = start;
+	constructor(start, duration, content) {
+		/* start time of clip */
+		this.start = start;
 
-        /* Duration time */
-        this.duration = durations;
+		/* Duration time */
+		this.duration = durations;
 
-        /* Either a single Note or a SoundFile or something */
-        this.content = content;
-    };
+		/* Either a single Note or a SoundFile or something */
+		this.content = content;
+	};
 }
 
 class Layer {
-    constructor(name, clips = [], instrument = 0) {
-        this.name = name;
+	constructor(name, clips = [], instrument = 0) {
+		this.name = name;
 
-        /* Instrument type, grand piano etc */
-        this.instrument = instrument;
+		/* Instrument type, grand piano etc */
+		this.instrument = instrument;
 
-        /* An array of Clips */
-        this.clips = clips;
+		/* An array of Clips */
+		this.clips = clips;
 
-    }
-    addClip(clip) {
-        this.clips.push(clip);
-    }
+	}
+	addClip(clip) {
+		this.clips.push(clip);
+	}
 }
